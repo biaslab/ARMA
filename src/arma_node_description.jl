f807c3e4bec383f1ac4a71662cc3a2ae927a7af3
pspannring <philipspannring@gmail.com>
2021-02-22 14:59:16 +0100

    Added averageEnergy computations
    Derived from ARMAu with u ~N(0,0)

--- src/arma_node_description.jl
+++ src/arma_node_description.jl
@@ -12,9 +12,9 @@ Description:
 
     y_k = θ'⋅[y_k-1, …, y_k-M1, e_k-1, … e_k-M2] + e_k
 
-    where M1 is the number of previous observations and M2 the number of previous residuals. 
+    where M1 is the number of previous observations and M2 the number of previous residuals.
     These histories are stored as the following vectors:
-    - z_k-1 = [y_k-1, …, y_k-M1]' 
+    - z_k-1 = [y_k-1, …, y_k-M1]'
     - r_k-1 = [e_k-1, …, e_k-M2]'.
 
     Assume y_k, z_k-1 and r_k-1 are observed and e_k ~ N(0, τ^-1).
@@ -59,5 +59,19 @@ function averageEnergy(::Type{AutoRegressiveMovingAverage},
                        marg_r::ProbabilityDistribution{Multivariate},
                        marg_τ::ProbabilityDistribution{Univariate})
 
-    error("not implemented yet")
+        my = unsafeMean(marg_y)
+        mz = unsafeMean(marg_z)
+        mr = unsafeMean(marg_r)
+        x = [mz; mr]
+
+        mθ = unsafeMean(marg_θ)
+        Vθ = unsafeCov(marg_θ)
+        mτ = unsafeMean(marg_τ)
+
+        aτ = marg_τ.params[:a]
+        bτ = marg_τ.params[:b]
+
+        temp1 = -(1. / 2.)*digamma(aτ).-(1. / 2.)*log(bτ).+(1. / 2.)*log(2*pi) # 0.5 log(2 pi) ~= 0.4
+        temp2 = (1. / 2.).* mτ.*(my.^2 .-2 .*my.*mθ'*x.+x'*(Vθ.+mθ*mθ')*x)
+        return (temp1.+temp2)[1]
 end
